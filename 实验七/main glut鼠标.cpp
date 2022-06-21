36c60f6afdba6ade31de86a30b8ae7dd6d7b71b4
xiaoyulonga <107870983+xiaoyulonga@users.noreply.github.com>
2022-06-21 14:24:02 +0800

    Add files via upload

---
+++ 实验七/main glut鼠标.cpp
@@ -0,0 +1,86 @@
+#include<stdlib.h>
+#include<math.h>
+#include<windows.h>
+#define GLUT_DISABLE_ATEXIT_HACK
+#include<gl/glut.h>
+#include<windows.h>
+#include<gl/glut.h>
+#include<stdlib.h>
+
+GLsizei winWidth = 400, winHeight = 300;
+GLint ptCtr = 0;
+class scrPt {
+public:
+    GLint x, y;
+};
+
+void init(void)
+{
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+void displayFcn(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void winReshapeFcn(GLint newWidth, GLint newHeight)
+{
+    glViewport(0, 0, newWidth, newHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, GLdouble(newWidth), 0.0, GLdouble(newHeight));
+
+    winWidth = newWidth;
+    winHeight = newHeight;
+}
+
+void drawLineSegment(scrPt endPt1, scrPt endPt2)
+{
+    glBegin(GL_LINES);
+    glVertex2i(endPt1.x, endPt1.y);
+    glVertex2i(endPt2.x, endPt2.y);
+    glEnd();
+}
+
+void polyline(GLint button, GLint action, GLint xMouse, GLint yMouse)
+{
+    static scrPt endPt1, endPt2;
+
+    if (ptCtr == 0) {
+        if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN) {
+            endPt1.x = xMouse;
+            endPt1.y = winHeight - yMouse;
+            ptCtr = 1;
+        }
+    }
+    else
+        if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN) {
+            endPt2.x = xMouse;
+            endPt2.y = winHeight - yMouse;
+            drawLineSegment(endPt1, endPt2);
+            endPt1 = endPt2;
+        }
+        else
+            if (button == GLUT_RIGHT_BUTTON)
+                exit(0);
+    glFlush();
+}
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(winWidth, winHeight);
+    glutCreateWindow("Draw Interactive Ployline");
+
+    init();
+    glutDisplayFunc(displayFcn);
+    glutReshapeFunc(winReshapeFcn);
+    glutMouseFunc(polyline);
+
+    glutMainLoop();
+    return 0;
+}
---
+++ 实验七/main反走样.cpp
@@ -0,0 +1,81 @@
+#include<stdlib.h>
+#include<math.h>
+#include<windows.h>
+#define GLUT_DISABLE_ATEXIT_HACK
+#include<gl/glut.h>
+
+#define x_Screen 800
+#define y_Screen    600
+#define little 50
+#define middle 20
+#define large   8
+void myBackground()
+{
+glClearColor(0.0,0.0,0.0,1.0);
+glColor3f(1.0,1.0,1.0);
+}
+
+void myDisplay()
+{
+glEnable ( GL_DEPTH_TEST );
+//如果没有抗锯齿，则点为方形的。如果我们启动抗锯齿设置，则点是一个圆点。
+glEnable(GL_POINT_SMOOTH);
+glEnable(GL_LINE_SMOOTH);
+glHint(GL_POINT_SMOOTH_HINT, GL_NICEST); // Make round points, not square points
+glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);  // Antialias the lines
+glEnable(GL_BLEND);
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+int i;
+glBegin(GL_POINTS);
+for(i=0;i<little;i++)
+glVertex2f(50.0+rand()%x_Screen,50.0+rand()%y_Screen);
+glEnd();
+glPointSize(2);
+glBegin(GL_POINTS);
+for(i=0;i<middle;i++)
+glVertex2f(50.0+rand()%x_Screen,50.0+rand()%y_Screen);
+glEnd();
+glPointSize(8);
+glBegin(GL_POINTS);
+for(i=0;i<large;i++)
+glVertex2f(50.0+rand()%x_Screen,50.0+rand()%y_Screen);
+glEnd();
+
+glBegin(GL_POLYGON);
+for(i=0;i<64;i++)
+glVertex2f(600+50.0*cos((float)i/10) ,500+50.0*sin((float)i/10) );
+glEnd();
+glLineWidth(3);
+glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+glEnable(GL_BLEND);
+glEnable(GL_LINE_SMOOTH);
+glBegin(GL_LINE_STRIP);
+for(i=0;i<19;i++){
+glVertex2f(rand()%10+i*70,rand()%50+50.0+(i%2)*80);
+}
+glEnd();
+glutSwapBuffers();
+}
+void myChange(int w,int h)
+{
+glViewport(0,0,w,h);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluOrtho2D(0.0,x_Screen,0.0,y_Screen);
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+}
+
+
+int main()
+{
+glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+glutInitWindowSize(x_Screen,y_Screen);
+glutCreateWindow("Star");
+glutDisplayFunc(myDisplay);
+glutReshapeFunc(myChange);
+myBackground();
+glutMainLoop();
+}
